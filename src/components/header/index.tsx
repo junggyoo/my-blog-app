368015470169cf5c64486f16b2f666646b4766b9
junggyoo <junggyoo.1@gmail.com>
2023-04-19 22:57:52 +0900

    feat: Header 컴포넌트 세팅

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,41 @@
+import Link from 'next/link';
+
+const MENU_ITEMS = [
+  {
+    label: 'Home',
+    href: '/',
+  },
+  {
+    label: 'About',
+    href: '/about',
+  },
+  {
+    label: 'Posts',
+    href: '/posts',
+  },
+  {
+    label: 'Contact',
+    href: '/contact',
+  },
+];
+
 export default function Header() {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className="py-4">
+      <nav className="flex justify-between items-center">
+        <div>
+          <Link href="/" className="text-2xl font-semibold">
+            Seize Blog
+          </Link>
+        </div>
+        <div className="flex gap-4">
+          {MENU_ITEMS.map((item) => (
+            <Link href={item.href} key={item.href} className="font-extralight">
+              {item.label}
+            </Link>
+          ))}
+        </div>
+      </nav>
     </header>
   );
 }
